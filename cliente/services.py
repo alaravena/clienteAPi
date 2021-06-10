953e6618f310a4007414a7d168fffadbe8b4b967
Alejandra Aravena Silva <al.aravena@profesor.duoc.cl>
2021-06-09 23:13:01 -0400

    mejora del try

--- cliente/services.py
+++ cliente/services.py
@@ -4,10 +4,11 @@ def generate_request(url, params={}):
     # requests.post(url, data, headers, timeout)
     try:
         response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return response.json()
     except requests.exceptions.ConnectionError as e:
         response = "No hay respuesta"
-    if response.status_code == 200:
-        return response.json()
+        return response
 
 def get_starWars():
     response = generate_request('https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/all.json')

ed706e2ad0b191852885a198144a1e992740acd8
Alejandra Aravena Silva <al.aravena@profesor.duoc.cl>
2021-06-09 23:09:36 -0400

    se mejora el except

--- cliente/services.py
+++ cliente/services.py
@@ -4,7 +4,7 @@ def generate_request(url, params={}):
     # requests.post(url, data, headers, timeout)
     try:
         response = requests.get(url, params=params)
-    except:
+    except requests.exceptions.ConnectionError as e:
         response = "No hay respuesta"
     if response.status_code == 200:
         return response.json()
